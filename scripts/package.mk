3564bafcd58d2b11704a06d8954e7579bb6669cf
Ryan Jennings <ryan@micrantha.com>
2021-04-16 21:34:59 -0700

    update package script

--- scripts/package.mk
+++ scripts/package.mk
@@ -5,6 +5,11 @@ HELPS += help-image
 image:
 	@build/package/create_image
 
+.PHONY: deploy
+deploy: image
+	@/usr/bin/ssh micrantha.com 'redeploy registry.gitlab.com/micrantha/web'
+
 .PHONY: help-image
 help-image:
 	@echo " image               build a docker image"
+	@echo " deploy              build and deploy the docker image
