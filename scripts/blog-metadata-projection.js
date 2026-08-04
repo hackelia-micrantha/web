76f7832cd5c99018dd517635b31d463840cd36f9
RJ <rybertjen@gmail.com>
2026-08-03 23:33:32 -0700

    Use isolated blog metadata formatter

--- scripts/blog-metadata-projection.js
+++ scripts/blog-metadata-projection.js
@@ -1,20 +1,6 @@
-import { readFile } from "node:fs/promises"
-import path from "node:path"
+import { buildBlogMetadataModule as buildRawBlogMetadataModule } from "./blog-content-inventory.js"
+import { formatBlogMetadataModule } from "./blog-metadata-format.js"
 
-import { format } from "prettier"
-
-import {
-  buildBlogMetadataModule as buildRawBlogMetadataModule,
-  repositoryRoot,
-} from "./blog-content-inventory.js"
-
-const prettierConfig = JSON.parse(
-  await readFile(path.join(repositoryRoot, ".prettierrc.json"), "utf8"),
-)
-
-export async function buildBlogMetadataModule(inventory) {
-  return format(buildRawBlogMetadataModule(inventory), {
-    ...prettierConfig,
-    parser: "typescript",
-  })
+export function buildBlogMetadataModule(inventory) {
+  return formatBlogMetadataModule(buildRawBlogMetadataModule(inventory))
 }
