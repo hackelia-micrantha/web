ff874ae9c5d5042d90493e5aa37a8dc7cbcee310
RJ <rybertjen@gmail.com>
2026-08-04 00:38:52 -0700

    Enforce private request caching at Pages adapter boundary

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,19 @@
 import { createRequestHandler } from "@remix-run/server-runtime"
 import * as build from "../build/index.js"
+import {
+  PRIVATE_NO_STORE_CACHE_CONTROL,
+  getRequestCacheControl,
+} from "../app/services/cache-policy.js"
 
 const handleRequest = createRequestHandler(build)
+
+function cloneResponse(response, headers) {
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  })
+}
 
 function ensureHtmlCharset(response) {
   const contentType = response.headers.get("Content-Type")
@@ -17,12 +29,24 @@ function ensureHtmlCharset(response) {
   const headers = new Headers(response.headers)
   headers.set("Content-Type", `${contentType}; charset=utf-8`)
 
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers,
-  })
+  return cloneResponse(response, headers)
 }
 
-export const onRequest = async (context) =>
-  ensureHtmlCharset(await handleRequest(context.request, context))
+function enforcePrivateRequestCaching(request, response) {
+  if (getRequestCacheControl(request) !== PRIVATE_NO_STORE_CACHE_CONTROL) {
+    return response
+  }
+
+  const headers = new Headers(response.headers)
+  headers.set("Cache-Control", PRIVATE_NO_STORE_CACHE_CONTROL)
+
+  return cloneResponse(response, headers)
+}
+
+export const onRequest = async (context) => {
+  const response = ensureHtmlCharset(
+    await handleRequest(context.request, context),
+  )
+
+  return enforcePrivateRequestCaching(context.request, response)
+}
