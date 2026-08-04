506658ef5b1e3de59e2a8ef4547ad5f487353587
RJ <rybertjen@gmail.com>
2026-08-03 17:21:15 -0700

    Type imported MDX content modules

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,6 +1,19 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
 
+import type { ComponentType } from "react"
+
 declare module "react-dom/server.browser" {
   export * from "react-dom/server"
+}
+
+declare module "*.mdx" {
+  export const attributes: unknown
+  export const filename: string
+
+  const Content: ComponentType<{
+    components?: Record<string, ComponentType<Record<string, unknown>>>
+  }>
+
+  export default Content
 }
